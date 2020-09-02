8fd806b4934d21ad77dbcd858ba4eb4f954d3bb3
“candice.zhang <zhanghongxia@china-goldcard.com>
2020-09-02 17:01:46 +0800

    提交代码

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,16 +1,31 @@
 //index.js
 //获取应用实例
 const app = getApp()
-
-Page({
+const params = {
   data: {
-    motto: 'Hello World',
+    motto: 'Hello World 小紅君',
     userInfo: {},
     hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    nodes: [{
+      name: 'div',
+      attrs: {
+        class: 'div_class',
+        style: 'line-height: 60px; color: red;'
+      },
+      children: [{
+        type: 'text',
+        text: "You never know what you're gonna get."
+      }]
+    }]
+  },
+  clickMe: function () {
+      wx.showToast({
+        title: '第一次开发小程序'
+      })
   },
   //事件处理函数
-  bindViewTap: function() {
+  bindViewTap: function () {
     wx.navigateTo({
       url: '../logs/logs'
     })
@@ -21,7 +36,7 @@ Page({
         userInfo: app.globalData.userInfo,
         hasUserInfo: true
       })
-    } else if (this.data.canIUse){
+    } else if (this.data.canIUse) {
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
       // 所以此处加入 callback 以防止这种情况
       app.userInfoReadyCallback = res => {
@@ -43,7 +58,7 @@ Page({
       })
     }
   },
-  getUserInfo: function(e) {
+  getUserInfo: function (e) {
     console.log(e)
     app.globalData.userInfo = e.detail.userInfo
     this.setData({
@@ -51,4 +66,5 @@ Page({
       hasUserInfo: true
     })
   }
-})
+}
+Page(params)
